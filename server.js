85ebd770caed516759905e58bb4509bca60c9250
Sigrid Nicole Ramos <sigridncl@gmail.com>
2019-02-20 01:53:23 +0400

    initial commit

---
+++ server.js
@@ -0,0 +1,66 @@
+/*
+--------------------------------------------------------------------------------
+Server for Face Detection App using Clarifai API.
+--------------------------------------------------------------------------------
+Endpoints:
+  
+1. /signin - Validates the details of the user with the database. 
+2. /image - Increments the number of image entries of the user in the database. 
+3. /imageurl - Handles the API call for face detection.
+4. /register - Inserts details of the new user in the database.
+5. /profile/:id - Currently not being used by the app but is supposed to be a profile page dedicated to the user. 
+--------------------------------------------------------------------------------
+Modules / Dependencies:
+1. express - web application server framework
+2. body-parser - middleware used to extract the body from the incoming requests, in this app JSON data type
+3. cors - middleware that can be used to enable CORS (Cross-origin resource sharing), during the development of this app allowing the front-end and back-end scripts communicate with each other from different ports.  
+4. clarifai - source of the face detection API
+5. bcrypt-nodejs - secures the user passwords by generating hashes
+6. pg - PostgreSQL, db management used
+7. nodemon - dev tool to automatically restart the app when changes are saved
+8. knex - SQL query builder used for Postgres
+--------------------------------------------------------------------------------
+*/
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile')
+const image = require('./controllers/image')
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'super',
+    database : 'smartbrain'
+  }
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/',(req,res)=>{
+  res.send(database.users);
+})
+
+app.post('/signin', (req,res) => {signin.handleSignin(req,res,db,bcrypt)})
+
+app.put('/image', (req, res) => {image.handleImage(req, res,db)})
+
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
+
+app.post('/register', (req,res) => {register.handleRegister(req, res, db, bcrypt)})
+
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req,res,db)})
+
+app.listen(3000, ()=>{
+  console.log('app is running on port 3000');
+})
