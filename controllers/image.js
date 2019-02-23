d345ad1285a585c14b85c4d6c1273a40ab5e3176
Sigrid Nicole Ramos <sigridncl@gmail.com>
2019-02-24 02:49:26 +0400

    update server

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
-  apiKey: 'af6a822785194872a6ad58084c54e5b3'
+  apiKey: 'your api'
  });
 
 const handleApiCall = (req,res) => {
